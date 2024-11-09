573799e76eb9b95182667c01eb3909ad3f83254e
Joseph Nguyen <joseph.h.nguyen@sjsu.edu>
2024-11-08 22:29:32 -0800

    Implement right side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Joseph' }
+  const loggedIn = { 
+    firstName: 'Joseph', 
+    lastName: 'Nguyen',
+    email: 'jthnguyen02@gmail.com' }
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
           <HeaderBox 
             type='greeting'
-            title='Welcome'
+            title='Welcome,'
             user={loggedIn?.firstName || 'Guest'}
-            subtext='Simplify your financial journey, one transaction at a time.'
+            subtext='Simplify your financial journey, one transaction at a time'
           />
 
           <TotalBalanceBox 
             accounts={[]}
             totalBanks={4}
-            totalCurrentBalance={100000000.81}
+            totalCurrentBalance={658.12}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 150}, {currentBalance: 508.12}]}/>
     </section>
   )
 }
